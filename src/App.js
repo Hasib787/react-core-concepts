d899bdbe4e7911fed21bd51cb940632ca5a97932
Hasib787 <mr.hasib45@gmail.com>
2021-02-26 01:37:19 +0600

    load data from the external api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const nayoks = [{ name: 'Sharukh', age: 55 }, { name: 'Salman', age: 52 }, { name: 'Amir', age: 53 }, { name: 'RajniKanth', age: 60 }]
+  const [nayoks, setNayoks] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setNayoks(data))
+  }, [])
+  // const nayoks = [{ name: 'Sharukh', age: 55 }, { name: 'Salman', age: 52 }, { name: 'Amir', age: 53 }, { name: 'RajniKanth', age: 60 }]
   return (
 
     <div className="App">
@@ -10,7 +16,7 @@ function App() {
       <header className="App-header">
         <MovieCounter></MovieCounter>
         {
-          nayoks.map(nk => <Nayok name={nk.name} age={nk.age}></Nayok>)
+          nayoks.map(nk => <Nayok name={nk.name} key={nk.id} age={nk.age} website={nk.website}></Nayok>)
         }
       </header>
     </div>
@@ -44,7 +50,8 @@ function Nayok(props) {
   return (
     <div style={nayokStyle}>
       <h1>Ami Nayok: {props.name}</h1>
-      <h3>I have done 10 movies {props.age} years</h3>
+      <h3>I have done 10 movies {props.age || 30} years</h3>
+      <h4>Website: {props.website}</h4>
     </div>
   )
 }

a491674d4597dc8359b359c7a06dae6b4566b8c5
Hasib787 <mr.hasib45@gmail.com>
2021-02-25 21:34:33 +0600

    added some changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@
 import './App.css';
 
 function App() {
+  const nayoks = ['Sharukh','Salman','Amir','RajniKanth' ]
   return (
     
     <div className="App">
       
       <header className="App-header">
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
+      <Nayok name={nayoks[1]} age='55'></Nayok>
+      <Nayok name="Razzak"></Nayok>
+      <Nayok name={nayoks[3]}></Nayok>
+      <Nayok name={nayoks[0]}></Nayok>
       </header>
     </div>
   );
 }
 
-function Nayok(){
+function Nayok(props){
   const nayokStyle ={
     border: '2px solid red',
     margin: "10px",
@@ -24,8 +25,8 @@ function Nayok(){
   }
   return (
     <div style={nayokStyle}>
-       <h1>Ami khol Nayok!!</h1>
-       <h3>I have done 10 movies</h3>
+       <h1>Ami Nayok: {props.name}</h1>
+       <h3>I have done 10 movies {props.age  || 45} years</h3>
     </div>
   )
 }
